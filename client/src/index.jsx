892b3ac7cc919455d267cd4b5d4bd863de23cf10
106 <hayalen@qq.com>
2017-01-19 17:35:11 +0800

    save

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,14 +5,13 @@ import {Router, browserHistory} from 'react-router';
 
 import rootRoute from './routes/init';
 import reducers from './reducers/init';
-import png from './assets/images/test.png';
 
 // Grab the state from a global variable injected into the server-generated HTML
 const preloadedState = window.__PRELOADED_STATE__;
-
 // Create Redux store with initial state
-const store = createStore(reducers, preloadedState)
+const store = createStore(reducers, preloadedState);
 let rootElement = document.getElementById('root');
+
 render((
 		<Provider store={store}>
 				<Router history={browserHistory} routes={rootRoute}/>
--- client/src/routes/components/Index.jsx
+++ client/src/routes/components/Index.jsx
@@ -1,36 +1,55 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router';
+import Helmet from 'react-helmet';
 
 import './../../assets/sass/word.scss';
-import png from './../../assets/images/test.png';
 
-@connect(
-	state => ({...state })
-)
+@connect(state => ({
+		...state
+}))
 export default class Index extends Component {
-	static fetchData() {
-		return Promise.all([
-			dispatch(asyncAction)
-		]);
-	}
-	componentWillMount() {
-		console.log("ok");
-	}
-	componentDidMount() {
-		console.log("end!!")
-	}
-	render() {
-		const {
-			value_set
-		} = this.props;
-		return (
-			<div className="word">
-				<p className="text"><Link to="/">This is Homepage.HAHAHAHAHAHAHAHAHAHAH</Link></p>
-				<p className="text"><Link to="/set">go to set</Link></p>
-				<p className="text"><Link to="/get">go to get</Link></p>
-				{this.props.children}
-			</div>
-		)
-	}
+		static fetchData() {
+				return Promise.all([
+						this
+								.props
+								.dispatch(asyncAction)
+				]);
+		}
+		componentWillMount() {
+				console.log("ok");
+		}
+		componentDidMount() {
+				console.log("end!!")
+		}
+		render() {
+				const {value_set} = this.props;
+				return (
+						<div className="word">
+								<Helmet
+										title="My Title"
+										titleTemplate="MySite.com - %s"
+										defaultTitle="My Default Title"
+										meta={[
+										{
+												name: 'description',
+												content: 'Helmet application'
+										}, {
+												property: 'og:type',
+												content: 'article'
+										}
+								]}/>
+								<p className="text">
+										<Link to="/">This is Homepage.HAHAHAHAHAHAHAHAHAHAH</Link>
+								</p>
+								<p className="text">
+										<Link to="/set">go to set</Link>
+								</p>
+								<p className="text">
+										<Link to="/get">go to get</Link>
+								</p>
+								{this.props.children}
+						</div>
+				)
+		}
 }
